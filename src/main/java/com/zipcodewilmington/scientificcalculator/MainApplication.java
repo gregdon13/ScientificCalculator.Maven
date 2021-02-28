19e39e2a2a3c3bd9e5bae1dfc51da5176b37502e
Chris Allen <Chris@zipcodes-MacBook-Pro-350.local>
2021-02-28 11:49:06 -0500

    Added ctof and ftoc operations

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -5,40 +5,37 @@ package com.zipcodewilmington.scientificcalculator;
  */
 public class MainApplication {
     public static void main(String[] args) {
-        double y;
-        double ans;
+        double currentlyDisplayed;
+        boolean isRunning = true;
 
         Console.println("Welcome to my calculator!");
 
-        y = Console.getDoubleInput("Please enter your first number or enter \"Memory\" for your stored value");
+        currentlyDisplayed = Console.getDoubleInput("Please enter your first number or enter \"Memory\" for your stored value");
 
         Calculator myCalc = new Calculator();
 
-        myCalc.drawScreen(y);
+        myCalc.drawScreen(currentlyDisplayed);
 
-        while(true) {
+        while(isRunning) {
 
             String op = Console.getStringInput("Please enter the operation you would like to perform:");
             if (op.equalsIgnoreCase("clear")) {
-                ans = 0;
-            } else if (op.equalsIgnoreCase("add") || op.equalsIgnoreCase("subtract") || op.equalsIgnoreCase("multiply") || op.equalsIgnoreCase("divide") || op.equalsIgnoreCase("exponential")) {
+                currentlyDisplayed = 0;
+            }
+            else if (op.equalsIgnoreCase("add") || op.equalsIgnoreCase("subtract") || op.equalsIgnoreCase("multiply") || op.equalsIgnoreCase("divide") || op.equalsIgnoreCase("exponential")) {
                 double secondNum = Console.getDoubleInput("Please enter your second number or enter \"Memory\" for your stored value");
-                ans = myCalc.performOperation(op, y, secondNum);
-            } else {
-                ans = myCalc.performOperation(op, y);
+                currentlyDisplayed = myCalc.performOperation(op, currentlyDisplayed, secondNum);
+            }
+            else {
+                currentlyDisplayed = myCalc.performOperation(op, currentlyDisplayed);
             }
 
-            myCalc.drawScreen(ans);
-            y = ans;
+            myCalc.drawScreen(currentlyDisplayed);
 
         }
 
 
 
-
-        //Console.println("The user input %s as a string", s);
-        //Console.println("The user input %s as a d", d);
-        //Console.println("The user input %s as a integer", i);
 
     }
 }
